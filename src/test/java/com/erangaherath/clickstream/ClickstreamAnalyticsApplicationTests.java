ffea487a0706f3caf69ff3191123c41ec5fbd93c
Eranga Herath <erangaherathofficial@gmail.com>
2026-02-14 13:24:40 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/erangaherath/clickstream/ClickstreamAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erangaherath.clickstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickstreamAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
